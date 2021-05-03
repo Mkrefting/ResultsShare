c4c2b93d7537353f35c377cded1497f4b5e9ee80
Max <maxkrefting>
2021-05-03 12:51:41 +0100

    Initial Commit

---
+++ ResultsShare/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ResultsShare
+//
+//  Created by Krefting, Max (PGW) on 03/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ResultsShare/ResultsShareApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResultsShareApp.swift
+//  ResultsShare
+//
+//  Created by Krefting, Max (PGW) on 03/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct ResultsShareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
